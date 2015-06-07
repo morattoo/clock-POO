031d488c92612f1295ebecf283d85c5d5bb4784a
javier morato <javiermorato@gmail.com>
2015-06-06 20:56:27 -0400

    first POO

--- js/main.js
+++ js/main.js
@@ -3,20 +3,23 @@
 (function() {
   var canvas = document.getElementById("canvas");
 
-  console.log(canvas);
   var ctx = canvas.getContext("2d");
   var radius = canvas.height / 2;
   ctx.translate(radius, radius);
-  radius = radius * 0.90
-  setInterval(drawClock, 1000);
+  radius = radius * 0.90;
 
-  function drawClock() {
-    drawFace(ctx, radius);
-    drawNumbers(ctx, radius);
-    drawTime(ctx, radius);
+  var Clock = function(ctx, radius){
+    this.ctx = ctx;
+    this.radius = radius;
+  };
+
+  Clock.prototype.drawClock = function () {
+    this.drawFace(this.ctx, this.radius);
+    this.drawNumbers(this.ctx, this.radius);
+    this.drawTime(this.ctx, this.radius, this);
   }
 
-  function drawFace(ctx, radius) {
+  Clock.prototype.drawFace = function (ctx, radius) {
     var grad;
     ctx.beginPath();
     ctx.arc(0, 0, radius, 0, 2*Math.PI);
@@ -35,7 +38,7 @@
     ctx.fill();
   }
 
-  function drawNumbers(ctx, radius) {
+  Clock.prototype.drawNumbers = function (ctx, radius) {
     var ang;
     var num;
     ctx.font = radius*0.15 + "px arial";
@@ -53,7 +56,7 @@
     }
   }
 
-  function drawTime(ctx, radius){
+  Clock.prototype.drawTime = function (ctx, radius){
       var now = new Date();
       var hour = now.getHours();
       var minute = now.getMinutes();
@@ -63,16 +66,16 @@
       hour=(hour*Math.PI/6)+
       (minute*Math.PI/(6*60))+
       (second*Math.PI/(360*60));
-      drawHand(ctx, hour, radius*0.5, radius*0.07);
+      this.drawHand(ctx, hour, radius*0.5, radius*0.07);
       //minute
       minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
-      drawHand(ctx, minute, radius*0.8, radius*0.07);
+      this.drawHand(ctx, minute, radius*0.8, radius*0.07);
       // second
       second=(second*Math.PI/30);
-      drawHand(ctx, second, radius*0.9, radius*0.02);
+      this.drawHand(ctx, second, radius*0.9, radius*0.02);
   }
 
-  function drawHand(ctx, pos, length, width) {
+  Clock.prototype.drawHand = function(ctx, pos, length, width) {
       ctx.beginPath();
       ctx.lineWidth = width;
       ctx.lineCap = "round";
@@ -82,4 +85,8 @@
       ctx.stroke();
       ctx.rotate(-pos);
   }
+
+
+  var clock = new Clock(ctx, radius);
+  setInterval(clock.drawClock(this), 1000);
 })();
